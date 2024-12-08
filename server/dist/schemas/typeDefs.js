d98e0114ff6b320b3aac28380ec0c962ffddcfc7
johnc365 <johnrcaldwell123@gmail.com>
2024-12-08 03:38:02 -0600

    Added mutations for interacting with the database to the server

--- server/dist/schemas/typeDefs.js
+++ server/dist/schemas/typeDefs.js
@@ -13,10 +13,21 @@ const typeDefs = `
     password: String!
   }
 
+  input MatchInput {
+  giver: String!
+  receiver: String!
+  }
+
   type SSGroup {
     groupId: ID!
     name: String!
     members: [String]
+    matches: [Match]
+  }
+
+  type Match {
+  giver: String!
+  receiver: String!
   }
 
   type Auth {
@@ -35,10 +46,10 @@ const typeDefs = `
   type Mutation {
     addUser(input: UserInput!): Auth
     login(email: String!, password: String!): Auth
-    createSSGroup(name: String!, members: [String]!): SSGroup
+    createSSGroup(name: String!, members: [String]!, matches: [MatchInput]): SSGroup
+    addMemberToGroup(groupId: ID!, member: String!): SSGroup
+    removeMemberFromGroup(groupId: ID!, member: String!): SSGroup
+    removeSSGroup(groupId: ID!): Boolean
     }
     `;
-// addMemberToGroup(groupId: ID!, member: String!): SSGroup
-// removeMemberFromGroup(groupId: ID!, member: String!): SSGroup
-// removeSSGroup(groupId: ID!): Boolean
 export default typeDefs;
