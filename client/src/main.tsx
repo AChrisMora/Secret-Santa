5c9346479a8f2e647c8ad3defe404abf8c88dd07
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-09 12:33:28 -0500

    more code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,39 +1,20 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login';
-import CreateGroup from './pages/CreateGroup';
-import RandomSelection from './pages/RandomSelection';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 
-// Custom 404 Error Page
-const NotFound: React.FC = () => (
-  <div>
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-    <a href="/">Go back to Home</a>
-  </div>
-);
-
-const router = createBrowserRouter([
-  {
-    path: '/', // Default page is Login
-    element: <Login />,
-    errorElement: <NotFound />, // Custom error page
-  },
-  {
-    path: '/login', // Explicit login route
-    element: <Login />,
-  },
-  {
-    path: '/create-group',
-    element: <CreateGroup />,
-  },
-  {
-    path: '/random-selection',
-    element: <RandomSelection />,
-  },
-]);
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+  ReactDOM.createRoot(rootElement).render(
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
